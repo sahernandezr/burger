0e22d0ce357b55c4749d424cf00b4751312d0e7f
sahernandezr <[E50846950+sahernandezr@users.noreply.github.comgit config --global user.email 50846950+sahernandezr@users.noreply.github.com>
2019-12-08 22:32:58 -0600

    initial file structure added

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Mac', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon and cheese', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken', false);

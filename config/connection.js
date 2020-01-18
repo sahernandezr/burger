c0b603405002b62c0958186f2e286e383511fc79
sahernandezr <[E50846950+sahernandezr@users.noreply.github.comgit config --global user.email 50846950+sahernandezr@users.noreply.github.com>
2020-01-17 22:56:14 -0600

    changes to connection

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,11 @@
 var mysql = require("mysql");
 
-// var connection = mysql.createConnection({
-//     host: process.env.DB_HOST,
-//     port: process.env.PORT || process.env.DB_PORT,
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASS,
-//     database: process.env.DB_NAME
-// });
-
 var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "LeicaMySQL911*",
-  database: "burgers_db"
+    host: process.env.DB_HOST,
+    port: process.env.PORT || process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
 });
 
 // Make connection.

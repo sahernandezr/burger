f898ddef78f46b86febe4c8d0eb42ef1434ea9e8
sahernandezr <[E50846950+sahernandezr@users.noreply.github.comgit config --global user.email 50846950+sahernandezr@users.noreply.github.com>
2020-02-04 20:12:53 -0600

    jawsdb added

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,22 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JawsDB_URL);
+} else {
+
+  connection = mysql.createConnection({
     host: process.env.DB_HOST,
     port: process.env.PORT || process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME
-});
+  });
+};
 
 // Make connection.
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;

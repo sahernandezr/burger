41f29fd7ea5180841f384e6fbf956ac90b435029
sahernandezr <[E50846950+sahernandezr@users.noreply.github.comgit config --global user.email 50846950+sahernandezr@users.noreply.github.com>
2020-02-08 11:57:53 -0600

    connection made to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -7,11 +7,13 @@ if (process.env.JAWSDB_URL) {
 } else {
 
   connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME
+    
+    host: "u28rhuskh0x5paau.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    port: 3306,
+    user: "b9ktve8fcg7qbaxm",
+    password: "ig39wx40kbakmps4",
+    database: "zg04c58nn9eotcoz"
+
   });
 };
 
